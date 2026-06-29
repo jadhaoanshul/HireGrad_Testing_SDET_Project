6ffdcf777668be2047d7eb966c74d7a6668283fb
Prasad <hariprasadjakkana@gmail.com>
2026-06-29 12:09:38 +0530

    StudentDashBoard updated

--- src/main/java/pages/student/StudentDashboardPage.java
+++ src/main/java/pages/student/StudentDashboardPage.java
@@ -72,7 +72,10 @@ public class StudentDashboardPage {
                 applicationSubmittedCount,
                 inReviewCount,
                 selectedCount,
-                eligibleRoleCount));
+                eligibleRoleCount,
+                browseJobs,
+                viewTracker,
+                completeProfile));
     }
 
     public boolean areRecentDetailsDisplayed() {
