2bb2ef7a75caa667dbf3d06fb6a1fa7c715d338b
--global <mihirshrestha4@gmail.com>
2026-06-29 12:27:07 +0530

    Modified AdminBaseTest

--- src/test/java/base/AdminBaseTest.java
+++ src/test/java/base/AdminBaseTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
@@ -16,5 +17,15 @@ public class AdminBaseTest extends BaseTest{
 
         loginPage.loginAsPlacementCell(username, password);
         log.info("Admin logged in successfully");
+    }
+
+    public void navigateToApplicationMngmntPage(){
+        driver.findElement(By.xpath("//a[@id='admin-layout-nav-/admin/applications']//*[name()='svg']")).click();
+        log.info("Navigate to Application Management page successfully");
+    }
+
+    public void navigateToReportAnalysisPage(){
+        driver.findElement(By.xpath("//a[@id='admin-layout-nav-/admin/reports']//*[name()='svg']")).click();
+        log.info("Navigate to Report Analysis page successfully");
     }
 }
